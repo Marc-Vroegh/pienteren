c098c406459449b5fb2a24f06c060773efdbd26a
Marc <marcvroegh@MacBook-Air-van-Marc.local>
2024-03-01 16:58:54 +0100

    added sidebar

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('sidebar')
+    @parent
+@endsection
+@section('content')
+    <h1>My Page content is here</h1>
+    <h1 class="text-black">sample text</h1>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,85 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite('resources/css/app.css')
+        <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css"
+        />
+    </head>
+    @section('sidebar')
+    <body class="bg-gray-600">
+    <div
+      class="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900"
+    >
+      <div class="text-gray-100 text-xl">
+        <div class="p-2.5 mt-1 flex items-center">
+          <h1 class="font-bold text-gray-200 text-[30px] ml-3">Dashboard</h1>
+        </div>
+        <div class="my-2 bg-gray-600 h-[1px]"></div>
+      </div> 
+      <div
+        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+      >
+        <i class="bi bi-house-door-fill"></i>
+        <span class="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
+      </div>
+      <div
+        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+      >
+        <i class="bi bi-bookmark-fill"></i>
+        <span class="text-[15px] ml-4 text-gray-200 font-bold">Bookmark</span>
+      </div>
+      <div class="my-4 bg-gray-600 h-[1px]"></div>
+      <div
+        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+        onclick="dropdown()"
+      >
+        <i class="bi bi-chat-left-text-fill"></i>
+        <div class="flex justify-between w-full items-center">
+          <span class="text-[15px] ml-4 text-gray-200 font-bold">Chatbox</span>
+          <span class="text-sm rotate-180" id="arrow">
+            <i class="bi bi-chevron-down"></i>
+          </span>
+        </div>
+      </div>
+      <div
+        class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold"
+        id="submenu"
+      >
+        <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+          Social
+        </h1>
+        <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+          Personal
+        </h1>
+        <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
+          Friends
+        </h1>
+      </div>
+      <div
+        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+      >
+        <i class="bi bi-box-arrow-in-right"></i>
+        <span class="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
+      </div>
+    </div>
+@show
+<div style="margin-left: 300px;" class="container bg-white visible">
+    @yield('content')
+</div>
+</body>
+</html>
+
+<script type="text/javascript">
+      function dropdown() {
+        document.querySelector("#submenu").classList.toggle("hidden");
+        document.querySelector("#arrow").classList.toggle("rotate-0");
+      }
+      dropdown();
+
+      function openSidebar() {
+        document.querySelector(".sidebar").classList.toggle("hidden");
+      }
+</script>

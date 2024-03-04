2a25b6ed408442941f04ea1d61cf5766815375b7
Marc <marcvroegh@MacBook-Air-van-Marc.local>
2024-03-04 16:40:19 +0100

    added sidebar for widgets, added button for show sidebar, cleaned up entries in dashboard sidebar, changed styling etc

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,14 +4,33 @@
     @parent
 @endsection
 @section('content')
+<div style="margin-left: 300px;" class="">
     <div style="width: 300px; height: 300px;" id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-    <div style="width: 300px; height: 300px;" id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
-    <div style="width: 278px; height: 278px;" id="drag1" draggable="true" ondragstart="drag(event)" class="container p-2 bg-gray-200">
+    <div style="width: 300px; height: 300px;" id="div2" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+    <div style="width: 278px; height: 278px;" id="drag1" draggable="true" ondragstart="drag(event)" class="rounded-lg container p-2 bg-stone-600">
     <h1>Temperatuur</h1>
     <div style="margin-left: -20px; margin-top: 65px;" class="flex justify-center items-center">
         <i style="font-size: 100px;" class="bi bi-thermometer"></i>
         <h1>29 graden</h1>
         </div>
     </div>
+    <div style="width: 278px; height: 278px;" id="drag2" draggable="true" ondragstart="drag(event)" class="rounded-lg container p-2 bg-stone-600">
+    <h1>CO2</h1>
+    <div style="margin-left: -20px; margin-top: 65px;" class="flex justify-center items-center">
+        <i style="font-size: 100px;" class="bi bi-thermometer"></i>
+        <h1>6 co2</h1>
+        </div>
+    </div>
+
+    <div style="display: none;" id="hideatstart" class="absolute right-0 top-20 text-white flex items-center justify-center">
+        <div style="height: 1000%;" class="bg-gray-500 p-5 rounded-xl">
+            <div style="width: 300px; height: 300px; border: 2px;" id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+            <div style="width: 300px; height: 300px; border: 2px;" id="div2" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+        </div>
+    </div>
+</div>
+
+
+ 
     
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,89 +3,97 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         @vite('resources/css/app.css')
+        <script src="https://code.jquery.com/jquery-latest.min.js"></script>
         <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css"
         />
     </head>
     @section('sidebar')
-    <body class="bg-gray-600">
-    <div
-      class="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900"
-    >
+    <body class="bg-gray-900">
+
+    <div class="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-black">
       <div class="text-gray-100 text-xl">
         <div class="p-2.5 mt-1 flex items-center">
           <h1 class="font-bold text-gray-200 text-[30px] ml-3">Dashboard</h1>
         </div>
         <div class="my-2 bg-gray-600 h-[1px]"></div>
       </div> 
+
       <div
-        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-      >
+        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
         <i class="bi bi-house-door-fill"></i>
         <span class="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
       </div>
-      <div
-        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-      >
-        <i class="bi bi-bookmark-fill"></i>
-        <span class="text-[15px] ml-4 text-gray-200 font-bold">Bookmark</span>
-      </div>
+     
       <div class="my-4 bg-gray-600 h-[1px]"></div>
-      <div
-        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-        onclick="dropdown()"
-      >
-        <i class="bi bi-chat-left-text-fill"></i>
+      <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+        onclick="dropdown()">
+
+        <i class="bi bi-gear-fill"></i>
         <div class="flex justify-between w-full items-center">
-          <span class="text-[15px] ml-4 text-gray-200 font-bold">Chatbox</span>
+          <span class="text-[15px] ml-4 text-gray-200 font-bold">Settings</span>
           <span class="text-sm rotate-180" id="arrow">
             <i class="bi bi-chevron-down"></i>
           </span>
         </div>
       </div>
-      <div
-        class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold"
-        id="submenu"
-      >
-        <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-          Social
-        </h1>
-        <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-          Personal
-        </h1>
-        <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-          Friends
+
+      <div class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu">
+        <h1 id="widget" class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1 widget">
+          Widgetbar tonen
         </h1>
       </div>
-      <div
-        class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-      >
+      <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
         <i class="bi bi-box-arrow-in-right"></i>
         <span class="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
       </div>
     </div>
 @show
-<div style="margin-left: 300px;" class="container p-5">
+<div style="" class="container p-5">
     @yield('content')
 </div>
 </body>
 </html>
+<script>
+  $(document).on("click",".widget", function () {
+   document.getElementById("hideatstart").style.display = 'block';
+  });
+</script>
 
 <script type="text/javascript">
+      function sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+          if ((new Date().getTime() - start) > milliseconds){
+            break;
+          }
+        }
+      }
+
       function allowDrop(ev) {
         ev.preventDefault();
       }
 
       function drag(ev) {
         ev.dataTransfer.setData("text", ev.target.id);
+        document.getElementById("hideatstart").style.display = 'block';
       }
 
       function drop(ev) {
         ev.preventDefault();
         var data = ev.dataTransfer.getData("text");
-        ev.target.appendChild(document.getElementById(data));
+        //alert(data);
+        //alert(ev.target.id);
+        if(!document.getElementById(ev.target.id).contains(document.getElementById('drag'))) {
+          ev.target.appendChild(document.getElementById(data));
+          document.getElementById("hideatstart").style.display = 'none';
+        }
       }
+
+      function hide() {
+      }
+
 
       function dropdown() {
         document.querySelector("#submenu").classList.toggle("hidden");

8f7e28d54aafd1d5fd9037902e0e12bcd71e05ba
Luna <99044668@mydavinci.nl>
2024-03-07 13:14:27 +0100

    fixed factory issue, and added factory to create test user.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name'); // Adding first name column
-            $table->string('last_name'); // Adding last name column
+            $table->string('first_name');
+            $table->string('last_name'); 
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

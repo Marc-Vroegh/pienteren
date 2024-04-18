81fcd3018f4b87c4a2467feef8dad796d9730068
Marc Vroegh <99078902@mydavinci.nl>
2024-04-18 20:12:09 +0200

    fixed styling hide scroll bar for windows users, also have fixed styling of the Widget Styler. also now has fixed width and height, i also have added a max length to the input to fix overflow issue

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,10 @@
 
 @section('content')
   
-<div class="dashboard-container hidescroll overflow-scroll">
+<div class="dashboard-container scrollbar-hide overflow-scroll">
 
     <!-- Dashboard -->
-    <div id="dashboard" class="p-3 overflow-scroll absolute hidescroll h-full">
+    <div id="dashboard" class="p-3 overflow-scroll absolute scrollbar-hide h-full">
         <div class="flex flex-wrap">
             <!-- Loop through amount of divs -->
             <?php for ($i = 1; $i <= 14; $i++) { ?>
@@ -19,7 +19,7 @@
     </div>
     
     <!-- Widgetbar -->
-    <div id="widget_container" class="widget_container absolute bottom-0 start-0 text-white overflow-scroll">
+    <div id="widget_container" class="widget_container absolute bottom-0 start-0 text-white scrollbar-hide overflow-scroll">
         <div class="widget-content bg-gray-300 p-5 rounded-t-2xl border-solid border-gray-400 border-2 opacity-90">
             <div class="flex flex-wrap">
                 <!-- Loop through amount of divs -->
@@ -48,20 +48,23 @@
 
 
     <!-- pop-up container for widget styling -->
-    <div id="pop-up-container" class="hidden right-0 top-0 w-full h-screen absolute bg-black bg-opacity-40">
-        <div id="pop-up-styler" class="hidden w-full absolute right-0 top-0 text-white black-container h-screen flex items-center">
-            <div style="color: white !important; width: 50%; height: 85%; margin-left: auto; margin-right: auto;" class="overflow-scroll bg-opacity-95 rounded-2xl bg-neutral-700 p-5">
-                <h1 class="text-lg">Widget Styler</h1>
-                    <div style="" class="p-6">
-                        <div style="width: 100%; height: 340px;" id="pop-up-inner-container" class="h-screen flex justify-center pop-up-inner-container">
+    <div id="pop-up-container" class="hidden right-0 top-0 w-full h-screen absolute bg-black bg-opacity-40 scrollbar-hide">
+        <div id="pop-up-styler" class="hidden w-full absolute right-0 top-0 text-white black-container h-screen flex items-center scrollbar-hide">
+            <div style="color: white !important; width: 700px; height: 580px; margin-left: auto; margin-right: auto;" class="scrollbar-hide overflow-scroll bg-opacity-95 rounded-2xl bg-neutral-700 p-5">
 
-                        </div>  
-                        <div style="" id="pop-up-inner-container-change" class="z-50 bg-neutral-900 p-5 rounded-xl pop-up-inner-container-change">
-                            <h1>Aanpassingen</h1>
-                            <div class="pt-3">
-                                <div class="grid grid-cols-3 gap-3">
-                                    <div>
-                                        <label for="box" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kies bron</label>
+                <h1 class="text-lg">Widget Styler</h1>
+
+                    <div class="p-6">
+                        <div style="width: 100%; height: 340px;" id="pop-up-inner-container" class="h-screen flex justify-center pop-up-inner-container">
+                    </div>  
+
+                    <div id="pop-up-inner-container-change" class="z-50 bg-neutral-900 p-5 rounded-xl pop-up-inner-container-change">
+                        <h1>Aanpassingen</h1>
+
+                        <div class="pt-3">
+                            <div class="grid grid-cols-3 gap-3">
+                                <div>
+                                    <label for="box" class="block mb-2 text-sm font-medium text-gray-900 text-white">Kies bron</label>
                                         <select id="box" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onchange="changeColor(this.value)">
                                             <option selected>Box1</option>
                                             <option value="box2">Box2</option>
@@ -69,16 +72,20 @@
                                             <option value="box4">Box4</option>
                                             <option value="box5">Box5</option>
                                         </select>
-                                    </div>
-                                    <div><label for="hs-color-input" class="block text-sm font-medium mb-2 dark:text-white">Kies kleur</label>
-                                    <input type="color" class=" h-10 w-full block bg-white cursor-pointer rounded-lg disabled:opacity-50 dis
-                                    abled:pointer-events-none dark:bg-gray-700 dark:border-gray-700" id="color-input" value="#2563eb" title="Choose your color" onchange="changeColor(this.value)"></div>
-                                    
-                                    <div><div class="grid gap-6 mb-6 md:grid-cols-2"><div><label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kies naam</label>
-                                    <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-300px p-2.5 dark:bg-gray-700 dark:bo
-                                    rder-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Choose name (required)" onchange="changeText(this.value)" required/></div></div>
                                 </div>
+
+                                <div><label for="hs-color-input" class="block text-sm font-medium mb-2 dark:text-white">Kies kleur</label>
+                                    <input type="color" class=" h-10 w-full block bg-white cursor-pointer rounded-lg disabled:opacity-50 dis
+                                    abled:pointer-events-none dark:bg-gray-700 dark:border-gray-700" id="color-input" value="#2563eb" title="Choose your color" onchange="changeColor(this.value)">
+                                </div>
+                                    
+                                <div><label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 text-white">Kies naam</label>
+                                    <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:bo
+                                    rder-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" maxlength="30" placeholder="Choose name (required)" onchange="changeText(this.value)" required/></div></div>
+                                </div>
+
                             </div> 
+                       </div>
                     </div>
             </div>
         </div>
